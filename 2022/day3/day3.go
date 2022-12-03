7fc159b6d4fa085366a3bab30b9af8dff173f5ad
Paul Ingles <paul@oobaloo.co.uk>
2022-12-03 11:48:08 +0000

    refactor day 3; didn't like bags

--- 2022/day3/day3.go
+++ 2022/day3/day3.go
@@ -16,61 +16,52 @@ func priority(b byte) int {
 	return int(b) % 32
 }
 
-type bag struct {
-	itemsDict map[byte]int
+// dictionary for lookups. won't count more than once per addition
+type uniqueDict struct {
+	d map[byte]int
 }
 
-func newBag(contents []byte) *bag {
-	// create a dict for each bag; we'll then merge together
-	b := &bag{}
-	b.itemsDict = make(map[byte]int, 52)
-
-	for _, item := range contents {
-		if b.itemsDict[item] == 0 {
-			b.itemsDict[item] += 1
-		}
-	}
-
-	return b
+func newDict(size int) *uniqueDict {
+	return &uniqueDict{d: make(map[byte]int, size)}
 }
 
-// addContents combines contents of the bags together
-func (b *bag) addContents(other ...*bag) {
-	for _, otherBag := range other {
-		for k, _ := range otherBag.itemsDict {
-			b.itemsDict[k] += 1
+// adds items; will only add item from items once
+func (d *uniqueDict) addItems(items []byte) {
+	// use a dict to make sure we process each from items only once
+	itemsDict := make(map[byte]bool, len(items))
+
+	for _, item := range items {
+		if itemsDict[item] {
+			continue // already processed it from this list
 		}
+
+		d.d[item] += 1
+		itemsDict[item] = true
 	}
 }
 
-// find the unique badge (where we have 3 of them)
-func (b *bag) badgeType() byte {
-	for k, v := range b.itemsDict {
-		if v == 3 {
-			return k
-		}
-	}
-	return 0
-}
+const dictionarySize = 52
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	counter := 0
-	bags := make([]*bag, 0)
 	sumOfPriorities := 0
 
+	dict := newDict(dictionarySize) // create a dictionary for the group
 	for scanner.Scan() {
-		bag := newBag(scanner.Bytes())
-		bags = append(bags, bag)
+		dict.addItems(scanner.Bytes())
 		counter += 1
 
 		if counter == 3 {
-			// join contents of the bags together to find the key
-			bags[0].addContents(bags[1:3]...)
-			sumOfPriorities += priority(bags[0].badgeType())
+			// find the one item thats been counted 3 times
+			for k, v := range dict.d {
+				if v == 3 {
+					sumOfPriorities += priority(k)
+				}
+			}
 
-			bags = nil
 			counter = 0
+			dict = newDict(dictionarySize)
 		}
 	}
 
