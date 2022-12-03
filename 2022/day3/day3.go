505c787438f2df42b5ef0e0da562a8c9813724c6
Paul Ingles <paul@oobaloo.co.uk>
2022-12-03 11:28:39 +0000

    implement part 2

--- 2022/day3/day3.go
+++ 2022/day3/day3.go
@@ -15,31 +15,66 @@ func priority(b byte) int {
 	return int(b) % 32
 }
 
+type bag struct {
+	itemsDict map[byte]int
+}
+
+func newBag(s string) *bag {
+	// create a dict for each bag; we'll then merge together
+	b := &bag{}
+	b.itemsDict = make(map[byte]int, 52)
+
+	for _, item := range []byte(s) {
+		if b.itemsDict[item] == 0 {
+			b.itemsDict[item] += 1
+		}
+	}
+
+	return b
+}
+
+// addContents combines contents of the bags together
+func (b *bag) addContents(other ...*bag) {
+	for _, otherBag := range other {
+		for k, _ := range otherBag.itemsDict {
+			b.itemsDict[k] += 1
+		}
+	}
+}
+
+// find the unique badge (where we have 3 of them)
+func (b *bag) badgeType() byte {
+	for k, v := range b.itemsDict {
+		if v == 3 {
+			return k
+		}
+	}
+	return 0
+}
+
+func (b *bag) String() string {
+	return fmt.Sprintf("%v", b.itemsDict)
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-
+	counter := 0
+	bags := make([]*bag, 0)
 	sumOfPriorities := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
+		bag := newBag(line)
+		bags = append(bags, bag)
+		counter += 1
 
-		// break contents into 2 compartments
-		left := line[:len(line)/2]
-		right := line[len(line)/2:]
+		if counter == 3 {
+			// join contents of the bags together to find the key
+			bags[0].addContents(bags[1:3]...)
+			sumOfPriorities += priority(bags[0].badgeType())
 
-		// use a dictionary to hold counts of each character
-		dict := make(map[byte]int, 52)
-
-		// store values for all of one compartment
-		for i := 0; i < len(right); i++ {
-			dict[right[i]] = 1
-		}
-
-		for i := 0; i < len(left); i++ {
-			if dict[left[i]] == 1 {
-				// found our item
-				sumOfPriorities += priority(left[i])
-				break
-			}
+			bags = nil
+			counter = 0
 		}
 	}
 
